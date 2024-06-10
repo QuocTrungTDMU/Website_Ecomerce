025c734664536923265b49166b6ce0381da5c532
QuocTrungTDMUD21 <trungdang01012003@gmail.com>
2024-06-10 10:55:26 +0700

    Done Card Product

--- e_ecomerce_system/src/components/CardComponent/CardComponent.jsx
+++ e_ecomerce_system/src/components/CardComponent/CardComponent.jsx
@@ -1,20 +1,54 @@
-import { Card } from 'antd'
-import React from 'react'
-import { StyleNameProduct, WrapperReportText } from './style'
-import {StarOutlined } from '@ant-design/icons';
+import React from "react";
+import {
+  StyleNameProduct,
+  WrapperCardStyle,
+  WrapperDiscountText,
+  WrapperPriceText,
+  WrapperReportText,
+} from "./style";
+import { StarOutlined } from "@ant-design/icons";
+import logo from "../../assets/image/logo.png";
 
 const CardComponent = () => {
   return (
-    <Card
-    hoverable
-    style={{ width: 240 }}
-    bodyStyle={{padding:'10px'}}
-    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-  >
-    <StyleNameProduct>Iphone 14 Promax </StyleNameProduct>
-    <WrapperReportText>4.7 <StarOutlined /></WrapperReportText>
-  </Card>
-  )
-}
+    <WrapperCardStyle
+      hoverable
+      // headStyle={{ width: "200px", height: "200px" }}
+      style={{ width: 200 }}
+      bodyStyle={{ padding: "10px" }}
+      cover={
+        <img
+          alt="example"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+        />
+      }
+    >
+      <img
+        src={logo}
+        style={{
+          width: "58px",
+          height: "14px",
+          position: "absolute",
+          top: -2,
+          left: -1,
+          borderTopLeftRadius: '3px'
+        }}
+        alt="logo"
+      />
+      <StyleNameProduct>Iphone 14 Promax </StyleNameProduct>
+      <WrapperReportText>
+        <span style={{ marginRight: "4px" }}>
+          <span>4.9</span>{" "}
+          <StarOutlined style={{ fontSize: "12px", color: "yellow" }} />
+        </span>
+        <span>| Đã bán 1000+</span>
+      </WrapperReportText>
+      <WrapperPriceText>
+        20.000.000d
+        <WrapperDiscountText>-5%</WrapperDiscountText>
+      </WrapperPriceText>
+    </WrapperCardStyle>
+  );
+};
 
-export default CardComponent
+export default CardComponent;
--- e_ecomerce_system/src/components/CardComponent/style.js
+++ e_ecomerce_system/src/components/CardComponent/style.js
@@ -1,15 +1,34 @@
+import { Card } from "antd";
 import styled from "styled-components";
 
-export const StyleNameProduct = styled.div`
-    font-weight: 400;
-    font-size: 12px;
-    line-height: 16px;
-    color: rgb(56,56,61)
+export const WrapperCardStyle = styled(Card)`
+  width: 200px;
+  & img: {
+    height: 200px;
+    width: 200px;
+  }
+`;
 
-`
+export const StyleNameProduct = styled.div`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  color: rgb(56, 56, 61);
+`;
 export const WrapperReportText = styled.div`
-    font-size: 11x;
-    color:rgb(128,128,137);
-    display: flex;
-    align-items: center;
-`
+  font-size: 11x;
+  color: rgb(128, 128, 137);
+  display: flex;
+  align-items: center;
+  margin: 6px 0 0;
+`;
+export const WrapperPriceText = styled.div`
+  color: rgb(255, 66, 78);
+  font-size: 16px;
+  font-weight: 500px;
+`;
+export const WrapperDiscountText = styled.span`
+  color: rgb(255, 66, 78);
+  font-size: 12px;
+  font-weight: 500px;
+`;
